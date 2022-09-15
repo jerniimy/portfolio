1ffb25e21b9fefef7088c10c6ccb858d2aa4964b
jerniimy <jerniimy@gmail.com>
2022-09-15 19:48:49 +0300

    feat: add scroll to footer by pressing 'workWithMeButton'

--- main.js
+++ main.js
@@ -8,3 +8,17 @@ function switchToDesign() {
   designButton.classList.add("active");
   frontendButton.classList.remove("active");
 }
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
