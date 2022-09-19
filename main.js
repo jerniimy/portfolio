51d62c31c6c77b29474f1886f3adb4464ea2fd2a
jerniimy <jerniimy@gmail.com>
2022-09-19 14:24:22 +0300

    fix: fix functions mouseover and mouseout

--- main.js
+++ main.js
@@ -2,39 +2,7 @@ let frontendButton = document.getElementById("frontendBlock");
 let designButton = document.getElementById("designBlock");
 let design = document.getElementById('design');
 let frontend = document.getElementById('frontend');
-let cinemaTitle = document.getElementById('cinema');
-let cinemaPoster = document.getElementById('cinemaPoster');
-let ringsTitle = document.getElementsById('rings');
-let ringsPoster = document.getElementById('ringsPoster');
-let planetTitle = document.getElementsById('planet');
-let planetPoster = document.getElementById('planetPoster');
-let cosmeticsTitle = document.getElementById('cosmetics');
-let cosmeticsPoster = document.getElementById('cosmeticsPoster');
 
-function cinemaHover() {
-  cinemaPoster.classList.add("activePoster");
-}
-function cinemaStop() {
-  cinemaPoster.classList.remove("activePoster");
-}
-function ringsHover() {
-  ringsPoster.classList.add("activePoster");
-}
-function ringsStop() {
-  ringsPoster.classList.remove("activePoster");
-}
-function planetHover() {
-  planetPoster.classList.add("activePoster");
-}
-function planetStop() {
-  planetPoster.classList.remove("activePoster");
-}
-function cosmeticsHover() {
-  cosmeticsPoster.classList.add("activePoster");
-}
-function cosmeticsStop() {
-  cosmeticsPoster.classList.remove("activePoster");
-}
 function switchToFrontend() {
   designButton.classList.remove("active");
   frontendButton.classList.add("active");
@@ -47,17 +15,28 @@ function switchToDesign() {
   design.classList.add("active");
   frontend.classList.remove("active");
 }
-const anchors = document.querySelectorAll('a[href*="#"]')
 
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-
-    const blockID = anchor.getAttribute('href').substr(1)
-
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  });
-}
+document.getElementById('cinema').addEventListener('mouseover', (e) => {
+  document.getElementById('cinemaPoster').style.opacity = '1';
+});
+document.getElementById('cinema').addEventListener('mouseout', (e) => {
+  document.getElementById('cinemaPoster').style.opacity = '0';
+});
+document.getElementById('rings').addEventListener('mouseover', (e) => {
+  document.getElementById('ringsPoster').style.opacity = '1';
+});
+document.getElementById('rings').addEventListener('mouseout', (e) => {
+  document.getElementById('ringsPoster').style.opacity = '0';
+});
+document.getElementById('planet').addEventListener('mouseover', (e) => {
+  document.getElementById('planetPoster').style.opacity = '1';
+});
+document.getElementById('planet').addEventListener('mouseout', (e) => {
+  document.getElementById('planetPoster').style.opacity = '0';
+});
+document.getElementById('cosmetics').addEventListener('mouseover', (e) => {
+  document.getElementById('cosmeticsPoster').style.opacity = '1';
+});
+document.getElementById('cosmetics').addEventListener('mouseout', (e) => {
+  document.getElementById('cosmeticsPoster').style.opacity = '0';
+});
